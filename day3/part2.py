bd3baa2357b0b6a1734ef82021c07829e123583b
vinicius <viniciusescuderoinstituto@gmail.com>
2020-01-05 13:15:51 +0100

    solved day 3 part 2 (day 3 is still bad)

---
+++ day3/part2.py
@@ -0,0 +1,77 @@
+import sys
+
+file_name = 'in'
+
+UP = (0, 1)
+DOWN = (0, -1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
+
+class Move:
+  def __init__(self, direction, distance):
+    self.direction = direction
+    self.distance = distance
+
+"""
+  It's pointless to make this a two-pass. The only reason why it's like that is because I tried something else before and didn't work.
+  I'm too lazy to change it sorry. Good news is that it works, but it's probably slow as hell.
+  So basically just remove Move and add the points as you parse the movements.
+"""
+def solve(a, b):
+  a_n = b_n = 0
+  a_m = b_m = 0
+  a_moves = []
+  b_moves = []
+
+  for move in a:
+    if move[0] == 'U':
+      a_moves.append(Move(UP, int(move[1:])))   
+    if move[0] == 'D':
+      a_moves.append(Move(DOWN, int(move[1:])))   
+    if move[0] == 'R':
+      a_moves.append(Move(RIGHT, int(move[1:])))   
+    if move[0] == 'L':
+      a_moves.append(Move(LEFT, int(move[1:])))   
+  
+  for move in b:
+    if move[0] == 'U':
+      b_moves.append(Move(UP, int(move[1:])))   
+    if move[0] == 'D':
+      b_moves.append(Move(DOWN, int(move[1:])))   
+    if move[0] == 'R':
+      b_moves.append(Move(RIGHT, int(move[1:])))   
+    if move[0] == 'L':
+      b_moves.append(Move(LEFT, int(move[1:])))   
+  
+  points = {} 
+  min_step = sys.maxsize
+
+  x = y = 0
+  step = 0
+  for move in a_moves:
+    for distance in range(move.distance):
+      x += move.direction[0]
+      y += move.direction[1]
+      step += 1
+      points[(x, y)] = step
+
+  x = y = 0
+  step = 0
+  for move in b_moves:
+    for distance in range(move.distance):
+      x += move.direction[0]
+      y += move.direction[1]
+      step += 1
+      if (x, y) in points:
+        min_step = min(min_step, points[(x,y)] + step)
+        print("cut in: ({x}, {y})".format(x=x, y=y))  
+        print("distance is: {}".format(points[(x, y)] + step))
+  
+  print("min step is {}".format(min_step))
+
+with open(file_name) as f:
+  first_wire = f.readline().split(',')
+  second_wire = f.readline().split(',')
+  
+  solve(first_wire, second_wire)
+
